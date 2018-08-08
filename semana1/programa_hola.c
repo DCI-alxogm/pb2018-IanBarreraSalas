7c810c13622548632e8a7ef8b6d2fd38a1f66a43
Barrera Salas Ian <IanBarreraSalas>
2018-08-08 09:25:33 -0500

    Subi mi primer programa

--- semana1/programa_hola.c
+++ semana1/programa_hola.c
@@ -1,7 +1,13 @@
 /*
-     Esto es un comentario. Me sirve para escribir información relevante del pro
+     Esto es un comentario whuu Vi. Me sirve para escribir información relevante del pro
 grama que estoy haciendo. Por  ejemplo:
      Este programa imprime la típica frase “hola mundo a la pantalla”
-     Creado el 06/08/2018 por …
+     Creado el 06/08/2018 por Ian Barrera Salas
     */
-
+#include<stdio.h>
+int main()
+{
+	printf("Hola Ian \n");
+	 printf ("Este es el primer programa del curso\n");
+	 }
+/*Termine de escribir el Programa*/
