5274e0f081660d2e810f3747588308539d4d8a1d
Barrera Salas Ian <IanBarreraSalas>
2018-08-15 08:56:50 -0500

    Ejercicios

---
+++ semana2/Edad.c
@@ -0,0 +1,16 @@
+/*
+ Pregunta edad prueba
+*/
+#include <stdio.h>
+int main()
+{
+
+	int edad; 
+
+	printf ("Cual es tu edad \n");
+	scanf ("%i", &edad);
+	printf("\nTu ead es: %i \n",edad);
+	return 0;
+
+} 
+
