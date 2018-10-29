2d6203efaf133c6dc9d4a4d1ca3fb755b6f1015f
IanBarreraSalas <42146304+IanBarreraSalas@users.noreply.github.com>
2018-10-29 09:07:46 -0600

    Update proyecto2.c

--- semana11/proyecto2.c
+++ semana11/proyecto2.c
@@ -1,22 +1,22 @@
 #include<stdio.h>
 int main()
 {
-	int i=0,c=0,YA,XA,N;
-	float izq,der,arr,abj;
+	int i=0,c=0,YA,XA,N; //inicializa contadores//
+	float izq,der,arr,abj; //iniciliza valores iniciales//
 	FILE*datos;
-	FILE*resultados;
+	FILE*resultados; 
 
-	datos = fopen("datos","r");
+	datos = fopen("datos","r"); //lectura de datos//
 	fscanf(datos,"%f %f %f %f %i", &izq,&der,&arr,&abj,&N);
 	fclose(datos);
 	printf("%f %f %f %f %i", izq,der,arr,abj,N);
-	float placa[N][N];
+	float placa[N][N]; //inicliaza en 0 array//
 	for(YA=0; YA<N; YA++){
                 for(XA=0; XA<N; XA++){
                         placa[XA][YA]=0;
                 }
         }
-
+// se asignan valores iniciales//
 	for(YA=0;YA<N;YA++){
 		placa[0][YA]=arr;
 		placa[N-1][YA]=abj;
@@ -25,11 +25,12 @@ int main()
 		placa[XA][0]=izq;
 		placa[XA][N-1]=der;
 	}
-	while(i<10){
-	while(placa[5][5]<izq-1){
-		     char buffer[32];
+	while(i<10){ //repite 10 veces para confirmar datos//
+	while(placa[5][5]<izq-1){ //asegura que sea manor al valor de equilibiro//
+		     char buffer[32]; //array nombre archivos//
         while(c<21) {
 
+		//calcula cada instante de propagacion del calor//
 	for(YA=1; YA<N-1; YA++){
 		for(XA=1; XA<N-1; XA++){
 			placa[XA][YA]=(placa[XA+1][YA]+placa[XA-1][YA]+placa[XA][YA+1]+placa[XA][YA-1])/4;
@@ -38,7 +39,7 @@ int main()
 
                 snprintf(buffer, sizeof(char)*32, "resultados%i", c);
                 c++;
-	
+	//imprime resultados en archivos diferentes//
 	resultados=fopen(buffer,"w");
 	for(YA=0; YA<N; YA++){
                 for(XA=0; XA<N; XA++){
